2bb74fcd136945192bc6105fc1516960ae825c4f
Dominika <rogala.dominika@interia.pl>
2022-01-09 17:02:44 +0100

    logo, navigation unit tests

--- client/src/app/core/side-navigation/side-navigation.component.spec.ts
+++ client/src/app/core/side-navigation/side-navigation.component.spec.ts
@@ -1,25 +1,37 @@
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
+import { TranslateMock, TranslateMockPipe } from 'src/app/tests';
 import { SideNavigationComponent } from './side-navigation.component';
 
 describe('SideNavigationComponent', () => {
-  let component: SideNavigationComponent;
-  let fixture: ComponentFixture<SideNavigationComponent>;
+    let component: SideNavigationComponent;
+    let fixture: ComponentFixture<SideNavigationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SideNavigationComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SideNavigationComponent, TranslateMockPipe],
+            providers: [{ provide: TranslateService, useClass: TranslateMock }],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SideNavigationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SideNavigationComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have 2 navigations item', () => {
+        const debugElement = fixture.debugElement;
+        const navigationItems: DebugElement[] = debugElement.queryAll(
+            By.css('.navigation--item')
+        );
+
+        expect(navigationItems.length).toBe(2);
+    });
 });
--- client/src/app/tests/index.ts
+++ client/src/app/tests/index.ts
@@ -1,2 +1,4 @@
 export * from './services/localStorage.mock';
 export * from './services/translate.mock';
+
+export * from './pipes/translatePipe.mock';
