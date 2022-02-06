33750b78ad3f7d57dcdb14421e591a8bfc22cb82
Dominika <rogala.dominika@interia.pl>
2022-02-06 11:22:31 +0100

    server initialized, user creation rest

---
+++ server/src/products/products.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductController {
+    constructor(private _products: ProductsService) {}
+
+    @Post()
+    async addProduct(@Body('title') prodTitle: string, @Body('desc') prodDesc: string, @Body('price') prodPrice: number) {
+        const generatedId = await this._products.insertProduct(prodTitle, prodDesc, prodPrice);
+        return { id: generatedId };
+    }
+
+    @Get()
+    async getAllProducts() {
+        const products = await this._products.getProducts();
+        return products;
+    }
+
+    @Get(':id')
+    async getProduct(@Param('id') prodId: string) {
+        const product = await this._products.getProduct(prodId);
+        return product;
+    }
+
+    @Patch(':id')
+    async updateProduct(@Param('id') prodId: string, @Body('title') prodTitle: string, @Body('desc') prodDesc: string, @Body('price') prodPrice: number) {
+        await this._products.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null;
+    }
+
+    @Delete(':id')
+    async removeProduct(@Param('id') prodId: string) {
+        await this._products.deleteProduct(prodId);
+        return null;
+    }
+}
---
+++ server/src/shared/validators/user.validator.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserValidator {
+    validateEmail(email: string): RegExpMatchArray {
+        return email
+            .toLowerCase()
+            .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    }
+}
